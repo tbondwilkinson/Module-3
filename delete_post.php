9c0f284e88d220499ecbf4f601ba5f794113ee87
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-06 16:19:09 -0500

    Tests

--- delete_post.php
+++ delete_post.php
@@ -5,18 +5,6 @@ session_start();
 require "database.php";
 
 if (isset($_GET["post_id"])) {
-	$stmt = $mysqli->prepare("DELETE FROM posts WHERE post_id = ?");
-
-	if(!$stmt){
-		printf("Query Prep Failed: %s\n", $mysqli->error);
-		exit;
-	}
-
-	$stmt->bind_param('s', $_GET["post_id"]);
-
-	$stmt->execute();
-	 
-	$stmt->close();
 
 	$stmt1 = $mysqli->prepare("DELETE FROM comments WHERE post_id = ?");
 
@@ -30,6 +18,19 @@ if (isset($_GET["post_id"])) {
 	$stmt1->execute();
 	 
 	$stmt1->close();
+
+	$stmt = $mysqli->prepare("DELETE FROM posts WHERE post_id = ?");
+
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+
+	$stmt->bind_param('s', $_GET["post_id"]);
+
+	$stmt->execute();
+	 
+	$stmt->close();
 
 	header("Location: main.php");
 
