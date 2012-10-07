e825365d4aa969a110902ef72b2394c63d006fd2
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-06 21:49:03 -0500

    Upvote

--- main.php
+++ main.php
@@ -6,14 +6,14 @@ session_start();
 
 // Take the user to the login screen if the user has yet to log in.
 if (!$_SESSION['logged_in']) {
-	echo "<a href='login.php'>Login!</a><br>";
+	echo "<a href='login.php'>Login!</a><br><br>";
 }
 
 if (isset($_GET['error']) and $_GET['error'] == "invalid_token") {
 	echo "Invalid token!<br>";
 }
 
-$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username FROM posts ORDER BY post_timestamp DESC");
+$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, vote FROM posts ORDER BY post_timestamp DESC");
 
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -22,11 +22,11 @@ if(!$stmt){
 
 $stmt->execute();
 
-$stmt->bind_result($post_timestamp, $post, $post_id, $username);
+$stmt->bind_result($post_timestamp, $post, $post_id, $username, $vote);
 
 while($stmt->fetch()){
-	echo "<a href='individual_story.php?post_id=" . $post_id . "'>" . trim_text($post, 50) . "</a><br>";
-	echo "By " . $username . "<br><br>";
+	echo "<a href='individual_story.php?post_id=" . $post_id . "'>" . htmlentities(trim_text($post, 50)) . "</a><br>";
+	echo "By " . htmlentities($username) . "\t\t\t" . "<a href='upvote.php'>&uarr</a>" . "<br><br>";
 }
 ?>
 <!DOCTYPE html>
