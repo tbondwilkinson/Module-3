d5730134a2d6cd94bd205c3da3e8e27599ec59ef
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-07 15:06:27 -0500

    Bugfix

--- database.php
+++ database.php
@@ -1,7 +1,5 @@
-<?php phpinfo(); ?>
-
 <?php
-$mysqli = new mysqli('localhost', 'php', 'php', 'module3');
+$mysqli = new mysql('localhost', 'php', 'php', 'module3');
  
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
