f0176d10643f8801d772d10d2e4c6bb04a0e2e5f
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-06 20:07:40 -0500

    Bugfix

--- edit_post_submit.php
+++ edit_post_submit.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
+
+require "database.php";
 
 // Verify the user's token.
 if ($_SESSION['token'] !== $_POST['token']) {
-	echo $_SESSION['token'];
-	echo $_POST['token'];
-	//header("Location: main.php?error=invalid_token");
+	header("Location: main.php?error=invalid_token");
 	exit;
 } 
 
