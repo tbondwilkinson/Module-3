7816a7eef9cc8acd9fa0996c62c0edfe04608996
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-07 15:08:58 -0500

    Testing

--- database.php
+++ database.php
@@ -1,5 +1,7 @@
+<?php phpinfo(); ?>
+
 <?php
-$mysqli = new mysql('localhost', 'php', 'php', 'module3');
+$mysqli = new mysqli('localhost', 'php', 'php', 'module3');
  
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
