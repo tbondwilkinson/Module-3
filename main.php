b59b95b38eb27bd7c1d1d65f25f59d7e87fc6381
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-10 12:15:55 -0500

    Committed

--- main.php
+++ main.php
@@ -13,24 +13,27 @@ if (isset($_GET['error']) and $_GET['error'] == "invalid_token") {
 }
 
 // Retrieve a list of the 30 most recent posts, and then sort them by their number of votes.
-$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, votes FROM 
-	(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY ? DESC");
 if (isset($_GET['sort'])) {
 	if ($_GET['sort'] == "author") {
-		$stmt->bind_param("s", "username");
+		$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, votes FROM 
+			(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY username DESC");
 	}
 	else if ($_GET['sort'] == "post") {
-		$stmt->bind_param("s", "post");
+		$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, votes FROM 
+			(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY post DESC");
 	}
 	else if ($_GET['sort'] == "votes") {
-		$stmt->bind_param("s", "votes");
+		$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, votes FROM 
+			(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY votes DESC");
 	}
 	else if ($_GET['sort'] == "time") {
-		$stmt->bind_param("s", "post_timestamp");
+		$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, votes FROM 
+			(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY post_timestamp DESC");
 	}
 }
 else {
-	$stmt->bind_param("s", "votes");
+	$stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, votes FROM 
+		(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY votes DESC");
 }
 
 if(!$stmt){
