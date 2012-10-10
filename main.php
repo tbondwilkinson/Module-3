1862b6ae7379a88bd426822b014572905550d204
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-10 12:04:20 -0500

    Sorting stories

--- main.php
+++ main.php
@@ -14,7 +14,24 @@ if (isset($_GET['error']) and $_GET['error'] == "invalid_token") {
 
 // Retrieve a list of the 30 most recent posts, and then sort them by their number of votes.
 $stmt = $mysqli->prepare("SELECT post_timestamp, post, post_id, username, votes FROM 
-	(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY votes DESC");
+	(SELECT post_timestamp, post, post_id, username, votes FROM posts ORDER BY post_timestamp DESC LIMIT 30) AS t ORDER BY ? DESC");
+if (isset($_GET['sort'])) {
+	if ($_GET['sort'] == "author") {
+		$stmt->bind_param("s", "username");
+	}
+	else if ($_GET['sort'] == "post") {
+		$stmt->bind_param("s", "post");
+	}
+	else if ($_GET['sort'] == "votes") {
+		$stmt->bind_param("s", "votes");
+	}
+	else if ($_GET['sort'] == "time") {
+		$stmt->bind_param("s", "post_timestamp");
+	}
+}
+else {
+	$stmt->bind_param("s", "votes");
+}
 
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -45,4 +62,14 @@ $stmt->close();
   		</form>
 	</div>
 <? } ?>
+	<div id="toggle-sort">
+		<form action="" method="GET">
+			Sort the posts by:<br>
+			<input type="radio" name="sort" value="author">Author<br>
+			<input type="radio" name="sort" value="post">Post Title<br>
+			<input type="radio" name="sort" value="votes">Votes<br>
+			<input type="radio" name="sort" value="time">Timestamp<br>
+			<input type="submit" value="Change sort!">
+		</form>
+	</div>
 </body>
